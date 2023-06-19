1a5b2fb18a4e96403c13809934610a8d8a355bd1
vineet <vineethkatkamdec@gmail.com>
2023-06-19 00:51:36 -0700

    TC_04

---
+++ src/Collections/CSet3.java
@@ -0,0 +1,22 @@
+package Collections;
+
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class CSet3 {
+	public static void main(String[] args) {
+		TreeSet<String> arry =new TreeSet<String>();
+		arry.add("Vineet");
+		arry.add("Katkam");
+		//arry.add("Katkam");
+		
+		
+		System.out.println(arry);
+		
+		Iterator<String> it=arry.iterator();
+		while(it.hasNext())
+		{
+			System.out.println(it.next());
+		}
+	}
+}
